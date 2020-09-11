2055b9e467e1280213642f6b1e62c0a10e86d605
ssurgutsky <ssurgutsky@gmail.com>
2020-09-11 15:27:35 +0700

    - upd

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,9 @@
 /* eslint no-eval: 0 */
 
 export default {
-  INDEXEDDB_VERSION: 5,
+  INDEXEDDB_VERSION: 6,
   INDEXEDDB_STORE_NAME: 'store_taxoman',
-  CACHE_ENABLED: false,
+  CACHE_ENABLED: true,
   GP_PRODUCT1_NAME: 'com.skystairgames.taxoman.coin1',
   GP_PRODUCT2_NAME: 'com.skystairgames.taxoman.episodes',
   ENABLE_SAVE_BUTTON: true,
