1529f29c69dd86b0c560bc3ffd9ebd83817d2b16
ssurgutsky <ssurgutsky@gmail.com>
2020-10-01 18:13:45 +0700

    - bbc mission

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,9 +2,9 @@
 
 export default {
   // Increase this on every update to reload assets for new on startup
-  GAME_VERSION: 14,
+  GAME_VERSION: 17,
   // Release = true
-  CACHE_ENABLED: true,
+  CACHE_ENABLED: false,
 
   GAME_VERSION_VAR_NAME: 'game_version',
   GAME_UPGRADE_POINT: 'GAME_UPGRADEPOINT',
